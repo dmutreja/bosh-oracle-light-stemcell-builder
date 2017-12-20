587f91426451ca6b73db52c408f599e5023ab751
dmutreja <dhiraj.mutreja@oracle.com>
2017-12-19 21:17:48 -0800

    Change the name of the subnet to create.

--- ci/terraform/network.tf
+++ ci/terraform/network.tf
@@ -15,7 +15,7 @@ resource "oci_core_security_list" "ci_public_all" {
 resource "oci_core_subnet" "director_subnet" {
   availability_domain = "${data.null_data_source.SetupConfig.inputs.ad_name}"
   cidr_block          = "${var.director_subnet_cidr}"
-  display_name        = "ci_director_subnet_${replace(data.null_data_source.SetupConfig.inputs.ad_name, "-", "_")}"
+  display_name        = "stemcell_ci_director_subnet_${replace(data.null_data_source.SetupConfig.inputs.ad_name, "-", "_")}"
   dhcp_options_id     = "${data.null_data_source.VCN.inputs.dhcp_options_id}"
   compartment_id      = "${data.null_data_source.SetupConfig.inputs.compartment_id}"
   vcn_id              = "${data.null_data_source.VCN.inputs.id}"
